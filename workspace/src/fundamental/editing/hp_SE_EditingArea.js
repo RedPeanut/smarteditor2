881d8fc0d8810b3e3e6169943ac714d3e9b08f0b
RedPeanut <zamong99@gmail.com>
2021-03-28 22:54:54 +0900

    PAINT 핸들러 추가

--- workspace/src/fundamental/editing/hp_SE_EditingArea.js
+++ workspace/src/fundamental/editing/hp_SE_EditingArea.js
@@ -46,7 +46,7 @@ nhn.husky.SE_EditingArea = jindo.$Class({
 	},
 
 	$ON_MSG_APP_READY: function() {
-		this.oApp.registerBrowserEvent(this.editingArea, "scroll", "EVENT_EDITING_AREA_SCROLL");
+		//this.oApp.registerBrowserEvent(this.editingArea, "scroll", "EVENT_EDITING_AREA_SCROLL");
 	},
 
 	$BEFORE_EVENT_EDITING_AREA_SCROLL: function() {
@@ -68,9 +68,52 @@ nhn.husky.SE_EditingArea = jindo.$Class({
 		});
 		this.doc.load(runs);
 		console.log("this.doc.frame.actualWidth() = " + this.doc.frame.actualWidth());
+		this.oApp.exec("EDITING_AREA_PAINT");
 	},
 
 	$AFTER_PASTE_HTML: function() {
 		console.log("$AFTER_PASTE_HTML is called...");
 	},
+	
+	$ON_EDITING_AREA_PAINT: function() {
+		//console.log("$ON_EDITING_AREA_PAINT is called...");
+
+		this.editingArea.style.width = 100+"%";
+		this.editingArea.style.height = this.appContainer.clientHeight - (this.menuBar.clientHeight + this.toolBar.clientHeight + this.ruler.clientHeight + this.statusBar.clientHeight) + "px";
+		
+		this.scrollView.style.width = this.editingArea.clientWidth+"px";
+		this.scrollView.style.height = this.editingArea.clientHeight+"px";
+
+		//A4=210x297(mm),Document=794x1123(px,macbook 15")
+		var documentWidth = Math.floor(210*3.78);
+		var documentHeight = Math.floor(297*3.78);
+		var padding = 20;
+
+		this.scrollViewHorizontal.style.width = documentWidth + padding*2 + "px";
+		this.scrollViewVertical.style.height = documentHeight + padding*2 + "px";
+
+		var availableWidth = documentWidth;
+		if (this.doc.width() != availableWidth)
+			this.doc.width(availableWidth);
+
+		var logicalWidth = this.editingArea.clientWidth,
+			logicalHeight = this.editingArea.clientHeight;
+
+		var dpr = Math.max(1, window.devicePixelRatio || 1);
+		
+		this.editCanvas.width = dpr * logicalWidth;
+		this.editCanvas.height = dpr * logicalHeight;
+		this.editCanvas.style.width = logicalWidth + 'px';
+		this.editCanvas.style.height = logicalHeight + 'px';
+		
+		//console.log("dpr = " + dpr);
+		var ctx = this.editCanvas.getContext("2d");
+		ctx.scale(dpr, dpr);
+
+		ctx.clearRect(0, 0, this.editingArea.clientWidth, this.editingArea.clientHeight);
+		//console.log("this.scrollView.scrollLeft = " + this.scrollView.scrollLeft);
+		//console.log("this.scrollView.scrollTop = " + this.scrollView.scrollTop);
+		//this.doc.draw(ctx, carota.rect(padding+this.scrollView.scrollLeft, padding+this.scrollView.scrollTop, logicalWidth-2*padding, logicalHeight-2*padding));
+		this.doc.draw(ctx, carota.rect(0, 0, this.editingArea.clientWidth, this.editingArea.clientHeight));
+	}
 });
